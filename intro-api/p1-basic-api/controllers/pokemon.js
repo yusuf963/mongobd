13d0fe52fcd3bcc26a9916deabae02fdecc7ce70
yusuf963 <mohammad963yusuf@gmail.com>
2021-02-12 23:21:29 +0000

    setting up controller for data file

--- intro-api/p1-basic-api/controllers/pokemon.js
+++ intro-api/p1-basic-api/controllers/pokemon.js
@@ -1,9 +1,29 @@
 import pokemonData from '../modles/pokemonData.js'
 
-const getPokemon = (_req, res) => {
+const getPokemon = (req, res) => {
   res.send(pokemonData)
-} 
+}
+
+const getSinglePokemon = (req, res) => {
+  res.send('single pokemon')
+}
+
+const makePokemon = (req, res) => {
+  const body = req.body
+  pokemonData.push(body)
+  res.send(body)
+}
+const deletPokemon = (req, res) => {
+  const deletedpokemon = Number(req.params.id)
+  const allPokemonsButNotTheDeletedOne = pokemonData.filter(pokemon => {
+    return pokemon.id === !deletedpokemon
+  })
+  res.send(allPokemonsButNotTheDeletedOne)
+}
 
 export default {
-  getPokemon
+  getPokemon,
+  getSinglePokemon,
+  makePokemon,
+  deletPokemon
 }
--- intro-api/p1-basic-api/index.js
+++ intro-api/p1-basic-api/index.js
@@ -1,19 +1,18 @@
 import express from 'express'
 import logger from './middleware/logger.js'
 import router from './views/router.js'
-console.log(router)
 
 const app = express()
 
 const startServer = () => {
   app.use(express.json())
-  app.use(logger)
-  console.log(logger)
+  // app.use(logger)
   app.use('/api', router)
-  //req is temproary converted into variable if you 
+  // with hiven now, req is temproary variable which mean there is now usage for it now, suger syntax
   app.use((_req, res) => {
     res.send('hello from starting server')
   })
-  app.listen(8000, () => console.log('server up and running on port 8000'))
+  const port = process.env.PORT || 8000
+  app.listen(port, () => console.log(`server up and running on port ${port}`))
 }
 startServer()
